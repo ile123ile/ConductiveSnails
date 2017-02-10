c693111cf7563c8e1a02af332cf23beea73af6cc
Slambamwam <ebkrosen@gmail.com>
2017-02-09 16:43:20 -0800

    Basic Front End Commit

---
+++ src/com/CSClub/ConductiveSnails/Screen.java
@@ -0,0 +1,22 @@
+package com.CSClub.ConductiveSnails;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Screen extends JApplet 
+{
+	public void init ()
+	{
+		Container c = getContentPane ();
+		c.setBackground(Color.GRAY);
+		this.setSize(new Dimension (600, 600));
+	}
+	public void paint (Graphics canvas)
+	{
+		super.paint (canvas);
+		canvas.setColor (Color.blue);
+		canvas.fillRect (500, 500, 20, 50);
+		canvas.setColor (Color.red);
+		canvas.fillRect (100, 100, 20, 50);
+	}
+}
