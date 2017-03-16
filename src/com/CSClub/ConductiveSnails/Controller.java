888834c18546abe29c6761366f4587b79b3015c7
Slambamwam <ebkrosen@gmail.com>
2017-03-16 16:14:41 -0700

    second controller update

--- src/com/CSClub/ConductiveSnails/Controller.java
+++ src/com/CSClub/ConductiveSnails/Controller.java
@@ -1,16 +1,33 @@
 package com.CSClub.ConductiveSnails;
+import java.awt.event.KeyEvent;
 
 public class Controller 
 {
+	private Player player = new Player (500, 500, 0);
 	public Controller ()
 	{
-		Player player = new Player ();
 	}
-	public void moveForward()
+
+	public void keyPressed (KeyEvent key)
 	{
-		if (isPressing (w))
+		if (key.getKeyCode() == KeyEvent.VK_W)
 		{
-			
+			player.setPos (player.getXPos(), player.getYPos () - 10);
+		}
+		else if (key.getKeyCode() == KeyEvent.VK_S)
+		{
+			player.setPos (player.getXPos(), player.getYPos () + 10);
+		}
+		else if (key.getKeyCode() == KeyEvent.VK_A)
+		{
+			player.setPos (player.getXPos() + 10, player.getYPos ());
+		}
+		else if (key.getKeyCode() == KeyEvent.VK_D)
+		{
+			player.setPos (player.getXPos() - 10, player.getYPos ());
+		}
+		if (key.getKeyCode() == KeyEvent.VK_SPACE)
+		{
 		}
 	}
 }
